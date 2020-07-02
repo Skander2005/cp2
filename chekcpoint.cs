b2d246f03f68c0ef3d7d40166f5c3f823dcab24f
Skander2005 <67702424+Skander2005@users.noreply.github.com>
2020-07-02 15:42:18 +0100

    Add files via upload

---
+++ chekcpoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chekcpoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {Debug.Log (Div (0f,5.9f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    string Div(float b,float a) {
+    if (b ==0 )
+    return "error";
+    else {
+    float c =  a/b;
+    return c.ToString() ;
+     }
+    }
+     }
+    
+
